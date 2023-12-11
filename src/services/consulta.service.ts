10d5c1e7e2713c1cc7d241fb2fd252a0ef85cebd
Marco <marcodavidtoledo@gmail.com>
2023-12-10 23:02:42 -0400

    Ultimos cambios

--- src/services/consulta.service.ts
+++ src/services/consulta.service.ts
@@ -14,24 +14,39 @@ const addConsulta = async (consulta: Consulta) => {
     const idInverter = consulta.idInverter;
     const consumoAnual = consulta.consumoAnual;
 
+    console.log("Consumo anual: " + consumoAnual);
     const consumoMensual = consumoAnual! / 12;
+    console.log("Consumo mensual: " + consumoMensual);
 
     const PanelFound = await PanelModel.findById({ _id: idPanel });
     const BatteryFound = await BatteryModel.findById({ _id: idBattery });
     const InverterFound = await InverterModel.findById({ _id: idInverter });
 
-    const cantidadPaneles = Math.ceil(consumoAnual! / PanelFound!.potenciaMaximaW);
+    const cantidadPaneles = Math.ceil(((consumoAnual! / 12) / 30) / PanelFound!.potenciaMaximaW);
+    console.log("Cantidad de paneles: " + cantidadPaneles);
     const potenciaNecesaria = consumoMensual / BatteryFound!.voltaje;
-    const cantidadBaterias = Math.ceil(potenciaNecesaria / BatteryFound!.capacidadAh);
+    console.log("Potencia necesaria: " + potenciaNecesaria);
+    const cantidadBaterias = Math.ceil((potenciaNecesaria / BatteryFound!.capacidadAh) / BatteryFound!.capacidadAh);
+    console.log("Cantidad de baterias: " + cantidadBaterias);
     const potenciaTotalPaneles = cantidadPaneles * PanelFound!.potenciaMaximaW;
+    console.log("Potencia total de paneles: " + potenciaTotalPaneles);
     const cantidadInversores = Math.ceil(potenciaTotalPaneles / InverterFound!.potenciaNominalW);
+    console.log("Cantidad de inversores: " + cantidadInversores);
 
     const precioTotal = cantidadPaneles * PanelFound!.precio + cantidadBaterias * BatteryFound!.precio + cantidadInversores * InverterFound!.precio;
+    console.log("Precio total: " + precioTotal);
 
     const precioConsumoMensual = consumoMensual * 0.5;
-    const consumoHr = (consumoMensual / 30) / 12;
-    const ahorroPotencial = (7 * (precioConsumoMensual) + (5 * (8 * (consumoHr * 0.5))) - 5 * (4 * (consumoHr * 0.5)))
-    const tiempoRecuperacion = precioTotal / ahorroPotencial;
+    console.log("Precio consumo mensual: " + precioConsumoMensual);
+    const consumoHr = ((consumoAnual! / 12) / 30) / 12;
+    console.log("Consumo por hora: " + consumoHr);
+    const ahorroPotencial = Math.ceil(7 * (precioConsumoMensual) + (5 * (30 * (8 * (consumoHr * 0.5)))) - 5 * (30 * (4 * (consumoHr * 0.5))))
+    console.log("Calculo de ahorro potencial_1: ", (5 * (30 * (8 * (consumoHr * 0.5)))));
+    console.log("Calculo de ahorro potencial_2: ", (5 * (30 * (4 * (consumoHr * 0.5)))));
+    console.log("Calculo de ahorro potencial_3: ", (7 * (precioConsumoMensual)));
+    console.log("Ahorro potencial: " + ahorroPotencial);
+    const tiempoRecuperacion = (ahorroPotencial / precioTotal).toFixed(2);
+    console.log("Tiempo de recuperacion: " + tiempoRecuperacion);
 
     const newConsulta = new ConsultaModel({
         idUser: consulta.idUser,
